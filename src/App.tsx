73b9d7c73eb57ea6b732f9a684f6d3d4f02166f4
root <root@LAPTOP-J99FST3R.localdomain>
2025-08-01 18:33:01 +1000

    Set up the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+// TODO: 
+// Add a bar + button -> where a user can input a link
+// Returns a clickable link with the ? after removed 
+
+// TODO: Can add 
+  return (
+    <>
+      <h1>PeakTok</h1>
+      {/* TODO: Add */}
+    </>
+  )
+}
+
+export default App
